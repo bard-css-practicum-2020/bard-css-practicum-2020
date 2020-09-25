c61da47fccbaf4106be20499a43de3a69e15a8da
Ian Glover <ianglover@ianglover.com>
2020-09-25 17:07:11 -0400

    Fix build errors

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,23 +1,37 @@
 import { useState, useEffect } from "react"
 import throttle from "lodash.throttle"
 
+const getInitialX = () => {
+  if (typeof document !== `undefined`) {
+    return document.body.getBoundingClientRect().left
+  }
+}
+
+const getInitialY = () => {
+  if (typeof document !== `undefined`) {
+    return document.body.getBoundingClientRect().top
+  }
+}
+
 export default () => {
   // Set a single object `{ x: ..., y: ..., direction: ... }` once on init
   const [scroll, setScroll] = useState({
-    x: document.body.getBoundingClientRect().left,
-    y: document.body.getBoundingClientRect().top,
+    x: getInitialX(),
+    y: getInitialY(),
     direction: "",
   })
 
   const listener = e => {
-    // `prev` provides us the previous state: https://reactjs.org/docs/hooks-reference.html#functional-updates
-    setScroll(prev => ({
-      x: document.body.getBoundingClientRect().left,
-      y: -document.body.getBoundingClientRect().top,
-      // Here we’re comparing the previous state to the current state to get the scroll direction
-      direction:
-        prev.y > -document.body.getBoundingClientRect().top ? "up" : "down",
-    }))
+    if (typeof document !== `undefined`) {
+      // `prev` provides us the previous state: https://reactjs.org/docs/hooks-reference.html#functional-updates
+      setScroll(prev => ({
+        x: document.body.getBoundingClientRect().left,
+        y: -document.body.getBoundingClientRect().top,
+        // Here we’re comparing the previous state to the current state to get the scroll direction
+        direction:
+          prev.y > -document.body.getBoundingClientRect().top ? "up" : "down",
+      }))
+    }
   }
 
   useEffect(() => {
